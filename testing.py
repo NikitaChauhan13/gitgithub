c3fc16adc9f2ce00200f3dcd8897f3668639c41e
Nikita <nikitachauhan7060@gmail.com>
2025-02-26 15:05:11 +0530

    updated file

--- testing.py
+++ testing.py
@@ -1,1 +1,11 @@
 print("helloguysss")
+
+
+
+# git remote add origin git@github.com:NikitaChauhan13/gitgithub.git
+#git remote -v
+
+#git remote set-url origin https://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@github.com/NikitaChauhan13/gitgithub.git
+# remote -v
+
+#git push origin master
